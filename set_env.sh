ef663d076e0da63f1e65a299623359e891a8f889
Minh Hieu <nmhieu03032000@gmail.com>
2023-10-22 16:33:26 +0700

    init commit for project 5

---
+++ set_env.sh
@@ -0,0 +1,7 @@
+
+export AWS_PROFILE=default
+export AWS_ACCESS_KEY=ASIAZ2AZAYBT262OXREL
+export AWS_SECRET_ACCESS_KEY=vA37dSa8ATtVKgF1Mx6j3bFBVBRuwkWnumdPhW5q
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
